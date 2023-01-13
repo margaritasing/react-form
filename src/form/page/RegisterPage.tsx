c72b3aa1c06e356ec69471a61bb6e669ec645c33
margaritasing <libiamfreitesm@gmail.com>
2023-01-13 16:30:01 -0300

    modifico este componente

--- src/form/page/RegisterPage.tsx
+++ src/form/page/RegisterPage.tsx
@@ -3,25 +3,25 @@ import { useForm } from "../hooks/useForm"
 
 const RegisterPage = () => {
 
-  const { formData, onChange, name, email,password1, password2} = useForm({
-      name:'',
-      email:'',
-      password1:'',
-      password2:''
-  });
-
- 
-
-  
- 
+  const { formData, 
+          onChange, 
+          name, 
+          email,
+          password1, 
+          password2, 
+          resetForm,
+          isValidEmail
+        } = useForm({
+                    name:'',
+                    email:'',
+                    password1:'',
+                    password2:''
+        });
 
   const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-
     console.log(formData)
   }
-
-
 
   return (
     <div>
@@ -33,7 +33,9 @@ const RegisterPage = () => {
             placeholder="Name"
             value={name}
             onChange={ onChange}
+            className={`${ name.trim().length <= 0 && 'has-error' }`}
              />
+             { name.trim().length <= 0 && <span>Este campo es obligatorio</span>}
 
         <input 
             name="email"
@@ -41,7 +43,9 @@ const RegisterPage = () => {
             placeholder="Email"
             value={email}
             onChange={ onChange}
+            className={`${ !isValidEmail(email) && 'has-error' }`}
              />
+              { !isValidEmail(email)  && <span>Este email no es valido</span>}
 
         <input 
             name="password1"
@@ -49,7 +53,10 @@ const RegisterPage = () => {
             placeholder="Password"
             value={password1}
             onChange={ onChange}
+            className={`${ password1.trim().length <= 0 && 'has-error' }`}
              />
+             { password1.trim().length <= 0 && <span>Este campo es obligatorio</span>}
+             { password1.trim().length <  6 && password1.trim().length > 0 && <span>La contraseña debe tener 6 caracteres </span>}
 
         <input 
             name="password2"
@@ -57,9 +64,14 @@ const RegisterPage = () => {
             placeholder="Repet Password"
             value={password2}
             onChange={ onChange}
+            className={`${ password2.trim().length <= 0 && 'has-error' }`}
              />
+             { password2.trim().length <= 0 && <span>Este campo es obligatorio</span>}
+             { password2.trim().length > 0 && password2 !== password1  && <span>Las contraseña debe ser igual</span>}
+
 
              <button type='submit'>Create</button>
+             <button type='button' onClick={resetForm}>Reset</button>
         </form>
     </div>
   )
